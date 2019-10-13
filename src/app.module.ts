9458e47ac3d362bf9f363e190343bb06bef4b415
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-10-13 09:57:52 +0700

    Setup typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
